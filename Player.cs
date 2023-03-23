d2b913d7693a51876a845b74bbb1be7a8e748631
hendrikbene <hendrikbene.perdok@gmail.com>
2023-03-23 16:57:12 +0100

    oke

--- Player.cs
+++ Player.cs
@@ -1,12 +1,14 @@
 using Godot;
-using System;
 
-public partial class new_script : Node
+public partial class Player : Area2D
 {
+	[Export]public int Speed = 400; // How fast the player will move (pixels/sec).
+	public Vector2 ScreenSize; // Size of the game window.
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		 	
+
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
